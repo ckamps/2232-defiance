a45b7dbdc3f95c6e21c82697150bd0aa946847c4
Christopher <cpkampmeier@gmail.com>
2024-02-20 21:33:03 -0600

    river levels

--- src/python/river-data.py
+++ src/python/river-data.py
@@ -6,4 +6,4 @@ df = pd.read_csv(r'..\..\data\mo-river-data-2010-2024.csv')
 fig = px.line(df, x = 'date', y = 'river-level', title='Missouri River Water Levels 2010-2024')
 #fig.show()
 
-fig.write_image(r'..\..\assets\img\river\river-levels.png', width=1200, height=350, scale=1)
+fig.write_image(r'..\..\assets\img\river\river-levels.png', width=1800, height=800, scale=1)
