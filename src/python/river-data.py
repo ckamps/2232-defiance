8b7329bdc06d29ab7d077efa034f6b93c3458cb6
Christopher <cpkampmeier@gmail.com>
2024-02-20 21:28:40 -0600

    Adding river data code

---
+++ src/python/river-data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv(r'..\..\data\mo-river-data-2010-2024.csv')
+
+fig = px.line(df, x = 'date', y = 'river-level', title='Missouri River Water Levels 2010-2024')
+#fig.show()
+
+fig.write_image(r'..\..\assets\img\river\river-levels.png', width=1200, height=350, scale=1)
